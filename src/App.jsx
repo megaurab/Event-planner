aee48341aace14d04722f83189330af162ad69f8
Gaurabjyoti Buragohain <gaurabburagohain2001@gmail.com>
2024-08-20 15:58:00 +0530

    css changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import Navbar from "./components/Navbar"
-import './App.css'
+import Navbar from "./components/Navbar";
+import EventList from "./components/EventList";
+import Footer from "./components/Footer";
+import JoinUs from "./components/JoinUs";
+import Hero from "./components/Hero"
+import "./App.css";
 
 function App() {
   return (
-    <Navbar />
-  )
+    <div className="bg-black overflow-hidden">
+      <div className="">
+        <Navbar />
+      </div>
+      <div className="bg-black ">
+        <Hero />
+      </div>
+      <div>
+        <div>
+          <EventList />
+          <JoinUs />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,47 +1,32 @@
 import { useState } from "react";
-import { LOGO } from "../utils/constants";
+import { close, menu } from "../assets";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
+  const [toggle, setToggle] = useState(false);
   return (
-    <div className="flex justify-between h-32 bg-gradient-to-r from-blue-400 to-blue-700 text-white shadow-2xl w-full sticky top-0">
-      <div className="logo-container pt-1 w-1/3">
-        <img className="w-32 rounded-full" src={LOGO} />
+    <nav className="flex justify-between w-full">
+      <div>
+        <img src=""></img>
       </div>
-      <div className="flex items-center w-1/3">
-        <ul className="flex">
-          <li className="mx-5 italic transition-all duration-500 ease-in-out md:max-w-full md:opacity-100 max-w-0 opacity-0 overflow-hidden">
-            Dashboard
-          </li>
-          <li className="mx-5 italic transition-all duration-500 ease-in-out md:max-w-full md:opacity-100 max-w-0 opacity-0 overflow-hidden">
-            Tasks
-          </li>
-          <li className="mx-5 italic transition-all duration-500 ease-in-out md:max-w-full md:opacity-100 max-w-0 opacity-0 overflow-hidden">
-            Guests
-          </li>
-          <li className="mx-5 italic transition-all duration-500 ease-in-out lg:max-w-full lg:opacity-100 max-w-0 opacity-0 overflow-hidden">
-            Schedule
-          </li>
-          <li className="mx-5 italic transition-all duration-500 ease-in-out xl:max-w-full xl:opacity-100 max-w-0 opacity-0 overflow-hidden">
-            Budget
-          </li>
-          <li className="mx-5 italic transition-all duration-500 ease-in-out 2xl:max-w-full 2xl:opacity-100 max-w-0 opacity-0 overflow-hidden">
-            Notes
-          </li>
-          <li className="mx-5 italic transition-all duration-500 ease-in-out 2xl:max-w-full 2xl:opacity-100 max-w-0 opacity-0 overflow-hidden">
-            Settings
-          </li>
+      <div className="flex">
+        <ul className="list-none sm:flex hidden justify-end flex-1">
+          <li className="mr-4 cursor-pointer text-white">Find Events</li>
+          <li className="mr-4 cursor-pointer text-white">Create Events</li>
+          <li className="mr-4 cursor-pointer text-white">Help Center</li>
+          <li className="mr-4 cursor-pointer text-white">Find my tickets</li>
+          <li className="mr-4 cursor-pointer text-white">Login</li>
+          <li className="mr-4 cursor-pointer text-white">SignUp</li>
         </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain"
+            onClick={() => setToggle((prev) => !prev)}
+          />
+        </div>
       </div>
-      <div className="items-center w-1/3 justify-end hidden md:flex">
-        <img className="w-32 rounded-full transition-all duration-500 ease-in-out 2xl:max-w-full 2xl:opacity-100 max-w-0 opacity-0 overflow-hidden" src={LOGO} />
-        <span className="mx-5 transition-all duration-500 ease-in-out 2xl:max-w-full 2xl:opacity-100 max-w-0 opacity-0 overflow-hidden">V</span>
-      </div>
-      <div className="flex md:hidden items-center pr-5">
-        <button>☰</button>
-      </div>
-    </div>
+    </nav>
   );
 };
 
